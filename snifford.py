a82a3a9c25e0cc2afac125c8a615103674751cd2
Aakash1417 <durairaj@ualberta.ca>
2024-12-25 18:06:21 -0700

    Enhance data handling and model training: add data caching, update model path, and improve training loop

--- snifford.py
+++ snifford.py
@@ -5,13 +5,15 @@ from src.train import train_model
 from src.utils import plot_confusion_matrix_terminal
 
 data_folder = 'data/csv/MachineLearningCVE'
+processed_data_path = 'data/ids_dataset1.pkl'
+model_path = 'results/initial_model.pth'
 
 # load and preprocess data
-data = load_and_preprocess_data(data_folder)
+data = load_and_preprocess_data(data_folder, processed_data_path)
 X_train, X_test, y_train, y_test = split_data(data)
 
 # train and evaluate model
-y_test, y_pred = train_model(X_train, y_train, X_test, y_test)
+y_test, y_pred = train_model(X_train, y_train, X_test, y_test, model_path)
 y_pred = np.array(y_pred).flatten()
 
 # visualize  confusion matrix
